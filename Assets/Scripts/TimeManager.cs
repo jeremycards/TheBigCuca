982dfe8359a793e58990f720fdb54f03ff74d9de
gcaino <german.caino@hotmail.com>
2019-01-26 17:54:32 -0300

    Add Timer

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour {
 
+    public Text timeText;
+    public float initialTime = 60f;
+    private float elapsedTime = 0f;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+    {
+        elapsedTime = initialTime;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        UpdateTime();	
 	}
+
+    private void UpdateTime()
+    {
+        elapsedTime -= Time.deltaTime;
+        UpdateTextTime();
+    }
+
+    private void UpdateTextTime()
+    {
+        float tempTime = Mathf.Floor(elapsedTime);
+        if (tempTime < 0)
+            tempTime = 0;
+        timeText.text = tempTime.ToString();
+    }
 }
