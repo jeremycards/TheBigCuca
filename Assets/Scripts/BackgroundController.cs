4e3a51bf737427ef4d4b5db8ffe4538846b548dc
gcaino <german.caino@hotmail.com>
2019-01-27 10:47:28 -0300

    Added Background

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    private GameObject player;
+
+    void LateUpdate()
+    {
+        if (!player)
+        {
+            player = GameObject.Find("playerPossum(Clone)");
+        }
+        
+    }
+}
