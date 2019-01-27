43ae346a4108a5a3b97256cbb62d41015594b363
gzardi <gonzalo.zardi@hasar.com>
2019-01-27 13:34:56 -0300

    buho

--- Assets/Buho.cs
+++ Assets/Buho.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Buho : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Animator animator;
+    public float speed = 10;
+    public Transform playerPos;
+    public bool oneatack;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponentInParent<Animator>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+        animator.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        float distanceToPlayer = Vector2.Distance(this.transform.position, playerPos.position);
+        if (distanceToPlayer < 4 && !oneatack)
+        {
+
+            oneatack = true;
+                animator.SetTrigger("Atack");
+
+            playerPos.SendMessage("TakeDamage");
+
+        }
+    }
 }
---
+++ Assets/BuhoSpawnerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuhoSpawnerScript : MonoBehaviour {
+
+    public float TimeToSpawn=5;
+    public float actualtime;
+    public GameObject prefab;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (actualtime > TimeToSpawn)
+        {
+
+
+            GameObject fox = Instantiate(prefab, GameObject.FindGameObjectWithTag("Player").transform.position - (new Vector3(20, -3, 0)), Quaternion.identity);
+            actualtime = 0;
+        }
+        else {
+            actualtime += Time.deltaTime;
+        }
+		
+	}
+}
